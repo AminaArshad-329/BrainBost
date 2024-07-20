a0099ff510bd5604a0de3fb6f4bb9dc8e387c5e7
AminaArshad-329 <aminaarshad0329@gmail.com>
2024-07-20 14:47:52 -0700

    Alembic Files

---
+++ alembic/env.py
@@ -0,0 +1,55 @@
+import os
+from logging.config import fileConfig
+
+import importlib
+from sqlalchemy import engine_from_config, pool
+
+from alembic import context
+
+# Load your SQLAlchemy models
+models_module_path = "BrainBoost.models"
+models_module = importlib.import_module(models_module_path)
+target_metadata = models_module.Base.metadata
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
